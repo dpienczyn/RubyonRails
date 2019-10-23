4f90ed0b38379c87179e9678c58c1e2abce1809b
dpienczyn <donia19881@wp.pl>
2019-10-23 13:55:17 +0200

    data base

--- db/migrate/20191010140857_create_posts.rb
+++ db/migrate/20191010140857_create_posts.rb
@@ -2,9 +2,9 @@ class CreatePosts < ActiveRecord::Migration[5.2]
   def change
     create_table :posts do |t|
       t.string :author
-      t.string :description
+      t.text :description
       t.string :title
-
+      t.integer :user_id
       t.timestamps
     end
   end
