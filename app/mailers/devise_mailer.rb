e6a9ede1fe801a87a3c1e53e835a2f870a05d306
dpienczyn <donia19881@wp.pl>
2019-10-29 19:50:45 +0100

    Welcome email

--- app/mailers/devise_mailer.rb
+++ app/mailers/devise_mailer.rb
@@ -1,7 +1,7 @@
 class DeviseMailer < Devise::Mailer
 
   def confirmation_instructions(record, token, opts={})
-    opts[:from] = 'pienczyn.d@gmail.com'
+    opts[:from] = 'akshay@rubyinrails.com'
     opts[:to] = 'no_reply@rubyinrails.com'
     super
   end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,7 +2,7 @@ class UserMailer < ApplicationMailer
 
   def welcome_email(user)
     @user = user
-    mail to: user.email, subject: "Witamy na naszej stronie"
+    mail to: user.email, subject: "Witamy na naszej stronie", from: 'pienczyn.d@gmail.com'
   end
   
 end
