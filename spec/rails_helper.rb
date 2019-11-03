00830f3ace1ad22c8a5bb1b3253b9861661fc9c1
Miłosz Osiński <mylosz@yahoo.pl>
2019-11-03 11:44:11 +0100

    Clean up specs

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,20 +1,14 @@
-require 'spec_helper'
-require 'shoulda/matchers'
-require 'support/factory_bot_rails'
-require 'rspec/rails'
-
-Shoulda::Matchers.configure do |config|
-  config.integrate do |with|
-    with.test_framework :rspec
-    with.library :rails
-  end
-end
 ENV['RAILS_ENV'] ||= 'test'
 
-require File.expand_path('../config/environment', __dir__)
+require File.expand_path('../../config/environment', __FILE__)
 
+abort('The Rails environment is running in production mode!') if Rails.env.production?
 
-abort("The Rails environment is running in production mode!") if Rails.env.production?
+require 'spec_helper'
+require 'factory_bot_rails'
+require 'support/factory_bot_rails'
+require 'shoulda/matchers'
+require 'rspec/rails'
 
 begin
   ActiveRecord::Migration.maintain_test_schema!
@@ -22,17 +16,17 @@ rescue ActiveRecord::PendingMigrationError => e
   puts e.to_s.strip
   exit 1
 end
+
 RSpec.configure do |config|
-  
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
-
-  
   config.use_transactional_fixtures = false
-
- 
   config.infer_spec_type_from_file_location!
-
- 
   config.filter_rails_from_backtrace!
- 
+end
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
 end
