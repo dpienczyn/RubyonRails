3525b38ae04814cbe1c9c7747ad64b305ffa83d6
dpienczyn <donia19881@wp.pl>
2019-11-13 17:41:55 +0100

    changes in PostController

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,23 +1,16 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_post, only: [:edit, :update, :destroy]
-  # GET /posts
-  # GET /posts.json
+
   def index
-    #@posts = Post.all
-    puts params[:search]
     @posts = Post.where("title ILIKE ?", "#{params[:search]}%")
                  .page(params[:page]).per(9)
-    puts @posts.count
   end
 
-  # GET /posts/1
-  # GET /posts/1.json
   def show
     @post = Post.find(params[:id])
     respond_to do |format|
       format.html
-      format.json { render json: @post }
       format.pdf do
         pdf = PostPdf.new(@post)
         send_data pdf.render, filename: "#{@post.title}.pdf",
@@ -35,53 +28,40 @@ class PostsController < ApplicationController
   end
 
   def create
-    @user = current_user
-    @post = @user.posts.build(post_params)
-      if @post.save
-        @subscribers = Subscriber.all
-          if @subscribers != NIL
-            @subscribers.each do |subscriber|
-            SubscriberMailer.new_post(subscriber, @post).deliver
-          end
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      @subscribers = Subscriber.all
+        @subscribers.all.each do |subscriber|
+          SubscriberMailer.new_post(subscriber, @post).deliver
         end
-        redirect_to @post, notice: 'Post was successfully created.'
-      else
-        render :new
-      end
-    end
-
-  def update
-    respond_to do |format|
-      if @post.update(post_params)
-        format.html { redirect_to @post, notice: 'Post was successfully updated.' }
-        format.json { render :show, status: :ok, location: @post }
-      else
-        format.html { render :edit }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
-      end
+        redirect_to @post, notice: 'Dodano nowy post.'
+    else
+      render :new
     end
   end
 
-  # DELETE /posts/1
-  # DELETE /posts/1.json
+  def update
+    if @post.update(post_params)
+    redirect_to @post, notice: 'Zaktualizowano post.'
+    else
+    render :edit
+    end
+  end
+
   def destroy
     @post.destroy
-    respond_to do |format|
-      format.html { redirect_to posts_url, notice: 'Post was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to posts_url, notice: 'Post został usunięty.'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_post
-      @post = current_user.posts.find_by(id: params[:id])
-      redirect_to posts_path, notice: "Nie jesteś uprawniony do edycji tego postu" if @post.nil?
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def post_params
-      params.require(:post).permit(:author, :description, :title, :image)
-    end
+  def set_post
+    @post = current_user.posts.find_by(id: params[:id])
+    redirect_to posts_path, notice: "Nie jesteś uprawniony do edycji tego postu" if @post.nil?
+  end
+
+  def post_params
+    params.require(:post).permit(:author, :description, :title, :image)
+  end
 
 end
