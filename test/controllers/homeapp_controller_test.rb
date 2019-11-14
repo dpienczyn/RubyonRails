7aabb2acf28ac42e5fb1101d596d41a53215761e
dpienczyn <donia19881@wp.pl>
2019-11-14 13:46:27 +0100

    delete test folder

--- test/controllers/homeapp_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class HomeappControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get homeapp_index_url
-    assert_response :success
-  end
-
-end
