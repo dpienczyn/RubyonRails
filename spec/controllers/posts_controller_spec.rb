1897274ae7bff2dcb441cedb3c89002d18178f88
dpienczyn <donia19881@wp.pl>
2019-11-15 19:24:23 +0100

    add examples for methods new, create, show, edit

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 RSpec.describe PostsController, type: :controller do
 
   let!(:posts) { create_list(:post, 5) }
-
-  let(:valid_session) { {} }
+  let(:post) { create(:post) }
+  let(:user) {create(:user)}
 
   describe "GET #index" do
     it "returns a success response" do
@@ -20,15 +20,46 @@ RSpec.describe PostsController, type: :controller do
   end
 
   describe "GET #show" do
-    it 'should success and render to edit page' do
+    it 'should success response' do
       get :show, params: { id: post.id }
-      expect(response).to have_http_status(200)
+      expect(response).to be_successful
+    end
+
+    it 'should render show page' do
+      get :show, params: { id: post.id }
+      expect(response).to render_template(:show)
     end
   end
 
   describe "GET #new" do
-    it 'should success and render to new post' do
-      get :new, params: {}, session: valid_session
+    it 'requires authentication' do
+      get :new
+      expect(response).to redirect_to(new_user_session_path)
+    end
+
+    it 'should success' do
+      sign_in(user)
+      get :new
+      expect(response).to be_successful
+    end
+
+    it 'should render new page' do
+      sign_in(user)
+      get :new
+      expect(response).to render_template(:new)
+    end
+
+    it 'should assign new post' do
+      sign_in(user)
+      get :new
+      expect(assigns(:post)).to be_a_new(Post)
+    end
+  end
+
+  describe "GET #create" do
+    fit "should success create" do
+      sign_in(user)
+      post :create, params: { post: { author: post.author, title: post.title, description: post.description } }
       expect(response).to be_successful
     end
   end
