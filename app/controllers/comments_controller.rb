b7979ac5c0a1a90d07d891652a1d2c97b3e84867
dpienczyn <donia19881@wp.pl>
2019-11-20 23:22:09 +0100

    added indents

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,26 +1,25 @@
 class CommentsController < ApplicationController
 	skip_before_action :authenticate_user!
 
-  def new
-  @post = Post.find(params[:post_id])
-  @comment = Comment.new
-  end
+	def new
+		@post = Post.find(params[:post_id])
+		@comment = Comment.new
+	end
 
-  def create
-    @post = Post.find(params[:post_id])
-    @comment = @post.comments.create(comment_params)
-    redirect_to post_path(@post), notice: 'Dodano nowy komentarz.'
-  end
+	def create
+		@post = Post.find(params[:post_id])
+		@comment = @post.comments.create(comment_params)
+		redirect_to post_path(@post), notice: 'Dodano nowy komentarz.'
+	end
 
-  def destroy
-    @post = Post.find(params[:post_id])
-    @comment = @post.comments.find(params[:id])
-    @comment.destroy
-    redirect_to post_path(@post), notice: 'Komentarz został usunięty.'
-  end
+	def destroy
+		@post = Post.find(params[:post_id])
+		@comment = @post.comments.find(params[:id])
+		@comment.destroy
+		redirect_to post_path(@post), notice: 'Komentarz został usunięty.'
+	end
 
-  def comment_params
-    params.require(:comment).permit(:comment, :body)
-  end
-
+	def comment_params
+		params.require(:comment).permit(:comment, :body)
+	end
 end
