85d41d98ae10b3eade4e5a9f1002b0510cca0811
dpienczyn <donia19881@wp.pl>
2019-11-20 23:19:44 +0100

    removed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,18 @@
 Rails.application.routes.draw do
+  get 'pages/about'
+  get 'pages/contact'
+  get 'pages/projects'
   get 'settings/index'
   get 'password_resets/new'
-  get 'project/index'
-  get 'projects/index'
-  get 'contact/index'
   resources :posts do
     resources :comments
   end
-  get 'homeapp/index'
   root 'posts#index'
-  get 'home/about'
-  get 'contact/index'
   devise_for :users
   resources :subscribers, only: :create
   namespace :api, defaults: { format: 'json' } do
     namespace :v1 do
-      resources :post, only: [:show, :index]
+      resources :posts, only: [:show, :index]
     end
   end
 end
