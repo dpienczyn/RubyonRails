b987a65144d73b7fb0a359dd04217a4c16d08dc8
dpienczyn <donia19881@wp.pl>
2019-11-23 00:50:04 +0100

    added tests

--- spec/pdfs/post_pdf_spec.rb
+++ spec/pdfs/post_pdf_spec.rb
@@ -3,7 +3,7 @@ require "rails_helper"
 RSpec.describe PostPdf, :type => :pdf do
   let(:post) { create(:post) }
 
-	describe "pdf" do
+  describe "pdf" do
     let(:pdf) { PostPdf.initialize(post) }
 
     xit "check title post" do
