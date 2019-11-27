f0b91952cbda74f684db7db7f59223ef2c26823f
dpienczyn <donia19881@wp.pl>
2019-11-27 17:40:24 +0100

    added tests for api - PostsController

--- spec/controllers/api/v1/posts_controller_spec.rb
+++ spec/controllers/api/v1/posts_controller_spec.rb
@@ -2,12 +2,58 @@ require 'rails_helper'
 
 RSpec.describe Api::V1::PostsController, type: :controller do
 
+  let(:user) {create(:user)}
+  let!(:posts) { create_list(:post, 5) }
+
   describe "GET #index " do
     let(:post) { create(:post) }
 
-    it "returns a successful 200 response" do
+    it "returns http status success" do
+      expect(response).to have_http_status(:success)
+    end
+
+    it "should success" do
       get :index, format: :json
       expect(response).to be_success
+    end
+  end
+
+  describe "POST #create" do
+    let(:user) {create(:user)}
+    let(:post_params) { attributes_for(:post) }
+
+    subject do
+      sign_in(user)
+      post :create, params: { post: post_params }
+    end
+
+    it "returns http status :created" do
+      subject
+      expect(response).to have_http_status(:created)
+    end
+
+    context "when invalid" do
+      let(:post_params) { attributes_for(:post, title: '') }
+
+      it "when title is empty" do
+        sign_in(user)
+        subject
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+    end
+
+    context "send email to subscriber" do
+      let(:subscriber) { create(:subscriber) }
+
+      subject do
+        sign_in(user)
+        post :create, params: { post: post_params }
+      end
+
+      it 'send email' do
+        expect { SubscriberMailer.new_post(subscriber, subject)}
+        .to change { ActionMailer::Base.deliveries.count }.by(1)
+      end
     end
   end
 end
