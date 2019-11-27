ab5a27d8aa58192c131602de62b89b3a4e2f391a
dpienczyn <donia19881@wp.pl>
2019-11-27 17:49:28 +0100

    added dependent: :destroy for posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :confirmable,
   :recoverable, :rememberable, :trackable, :validatable
-  has_many :posts
+  has_many :posts, dependent: :destroy
 
   after_create :welcome_email
 
