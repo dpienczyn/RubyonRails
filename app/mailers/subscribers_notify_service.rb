8211b62a87329d1da00d66ee8d1c02b8566869d1
dpienczyn <donia19881@wp.pl>
2019-11-29 15:55:46 +0100

    changed file names

---
+++ app/mailers/subscribers_notify_service.rb
@@ -0,0 +1,8 @@
+class SubscribersNotifyService < ApplicationMailer
+
+  def new_post(subscriber, post)
+		@subscriber = subscriber
+		@post = Post.last(1)
+		mail to: subscriber.email, subject: "Nowy Post", from: 'pienczyn.d@gmail.com'
+	end
+end
