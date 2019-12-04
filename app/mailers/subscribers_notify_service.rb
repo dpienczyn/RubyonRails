4ae599f09a81d11f74746e39438048f288486885
dpienczyn <donia19881@wp.pl>
2019-12-04 14:18:27 +0100

    change Post.last on post

--- app/mailers/subscribers_notify_service.rb
+++ app/mailers/subscribers_notify_service.rb
@@ -2,7 +2,7 @@ class SubscribersNotifyService < ApplicationMailer
 
   def new_post(subscriber, post)
     @subscriber = subscriber
-		@post = Post.last
-		mail to: subscriber.email, subject: "Nowy Post", from: 'pienczyn.d@gmail.com'
-	end
+    @post = post
+    mail to: subscriber.email, subject: "Nowy Post", from: 'pienczyn.d@gmail.com'
+  end
 end
