9330ff8971373f8a75315a5bdb2f6d501f54c3ee
dpienczyn <donia19881@wp.pl>
2019-12-04 12:32:51 +0100

    added spaces

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,26 +1,26 @@
 class CommentsController < ApplicationController
-	skip_before_action :authenticate_user!
-	before_action :set_comment, only: [:create, :destroy]
+  skip_before_action :authenticate_user!
+  before_action :set_comment, only: [:create, :destroy]
 
-	def create
-		@comment = @post.comments.create(comment_params)
-		if @comment.save
-			redirect_to @post, notice: 'Dodano nowy komentarz.'
-		end
-	end
+  def create
+    @comment = @post.comments.create(comment_params)
+    if @comment.save
+      redirect_to @post, notice: 'Dodano nowy komentarz.'
+    end
+  end
 
-	def destroy
-		@comment = @post.comments.find(params[:id])
-		if @comment.destroy
-			redirect_to @post, notice: 'Komentarz został usunięty.'
-		end
-	end
+  def destroy
+    @comment = @post.comments.find(params[:id])
+    if @comment.destroy
+      redirect_to @post, notice: 'Komentarz został usunięty.'
+    end
+  end
 
-	def set_comment
-		@post = Post.find(params[:post_id])
-	end
+  def set_comment
+    @post = Post.find(params[:post_id])
+  end
 
-	def comment_params
-		params.require(:comment).permit(:comment, :body)
-	end
+  def comment_params
+    params.require(:comment).permit(:comment, :body)
+  end
 end
