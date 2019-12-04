9241933c2ebcdeae9c825b0f82a945a7d96970ce
dpienczyn <donia19881@wp.pl>
2019-12-04 14:16:15 +0100

    deleted method assign_user

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::SessionsController < Api::ApplicationController
+  before_action :authenticate_user!, only: :destroy
 
   def create
     user = User.find_for_database_authentication(email: user_params[:email])
@@ -7,28 +8,19 @@ class Api::V1::SessionsController < Api::ApplicationController
       render json: user.reload
     else
       render status: :unauthorized,
-        json: I18n.t('devise.sessions.invalid').to_json
+      json: I18n.t('devise.sessions.invalid').to_json
     end
   end
 
   def destroy
-    if user = assign_user
-      user.assign_auth_token!
-      render json: I18n.t('devise.sessions.user.signed_out').to_json
-    else
-      render json: {}, status: :unauthorized
-    end
+    @user.assign_auth_token!
+    render json: I18n.t('devise.sessions.user.signed_out').to_json
   end
 
   private
-  
+
   def user_params
     params.require(:user).permit(:email, :password)
-  end
-
-  def assign_user
-    token = request.env['HTTP_USER_TOKEN']
-    User.find_by(auth_token: token) if token
   end
 
   def sign_in(user)
